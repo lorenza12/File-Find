997a330185c15cd423a22b38a57b80b14baaa433
Alik <38536682+lorenza12@users.noreply.github.com>
2019-09-01 23:16:16 -0500

    Initial commit

---
+++ File Find/MainWindow.xaml.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Media.Animation;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace File_Find
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Searcher searcher = new Searcher();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void browse_btn_Click(object sender, RoutedEventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (!string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    directory_txtbx.Text = fbd.SelectedPath.ToString();
+
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                    // System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+
+
+                    searcher.SearchDirectory = fbd.SelectedPath.ToString();
+
+                    // System.Windows.Forms.MessageBox.Show(searcher.SearchDirectory.ToString());
+
+                }
+
+            }
+
+            Enable_Search_Btn();
+        }
+
+
+
+        private void search_txtbx_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Enable_Search_Btn();
+
+        }
+
+        private void fileType_cmbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            searcher.FileType = searcher.GetFileType((e.AddedItems[0] as ComboBoxItem).Content.ToString());
+
+            EnforceStipulations();
+
+            // System.Windows.Forms.MessageBox.Show(searcher.FileType);
+
+            Enable_Search_Btn();
+
+
+        }
+
+        private void Enable_Search_Btn()
+        {
+
+            search_btn.IsEnabled = ((directory_txtbx.Text != null && directory_txtbx.Text.Length > 0) &&
+                                    (search_txtbx.Text != null && search_txtbx.Text.Length > 0) &&
+                                    (fileType_cmbx.SelectedItem != null && fileType_cmbx.SelectedItem.ToString().Length > 0));
+
+        }
+
+        private void directory_txtbx_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (directory_txtbx.Text.Length > 0)
+            {
+                searcher.SearchDirectory = directory_txtbx.Text.Trim();
+            }
+
+
+
+            Enable_Search_Btn();
+
+        }
+
+        private async void search_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                EnforceStipulations();
+
+                statusError_lbl.Content = "";
+                loading_prgbar.IsIndeterminate = true;
+                loading_prgbar.Visibility = Visibility.Visible;
+
+                SetSearcherCheckboxes();
+
+                searcher.SearchWord = search_txtbx.Text.Trim();
+
+                await Task.Run(() => searcher.SearchForFiles());
+
+                //After trying to search, set any error messages
+                //SetErrorMessage(searcher.errorMessage);
+                statusError_lbl.Content = searcher.errorMessage;
+
+                foundFiles_lstbx.Items.Clear();
+
+                if (searcher.FoundFiles != null && searcher.FoundFiles.Length > 0)
+                {
+                    foreach (string file in searcher.FoundFiles)
+                    {
+                        if (file != null)
+                        {
+                            foundFiles_lstbx.Items.Add(file.ToString());
+                        }
+                    }
+
+                }
+
+                fileFoundCount_lbl.Content = "Files Found: " + searcher.FoundFiles.Length.ToString();
+
+
+                loading_prgbar.IsIndeterminate = false;
+                loading_prgbar.Visibility = Visibility.Collapsed;
+            }
+            catch (Exception ex)
+            {
+                loading_prgbar.IsIndeterminate = true;
+                loading_prgbar.Visibility = Visibility.Visible;
+                statusError_lbl.Content = "Error: " + ex.ToString();
+            }
+
+
+        }
+
+        private void EnforceStipulations()
+        {
+            if (searcher.FileType == ".csv")
+            {
+                matchWholeWord_chbx.IsEnabled = false;
+                matchWholeWord_chbx.IsChecked = false;
+                wholeWord_lbl.IsEnabled = false;
+
+                statusError_lbl.Content = "Note: Match whole word functionality doesn't properly work on csv formats";
+            }
+
+            else if (searcher.FileType == ".*")
+            {
+
+                statusError_lbl.Content = "Note: Find in files functionality doesn't work on all file types";
+
+                //if we switched from csv to all files
+                matchWholeWord_chbx.IsEnabled = true;
+                wholeWord_lbl.IsEnabled = true;
+            }
+            else
+            {
+
+                matchWholeWord_chbx.IsEnabled = true;
+                wholeWord_lbl.IsEnabled = true;
+                statusError_lbl.Content = "";
+            }
+        }
+
+        private void SetSearcherCheckboxes()
+        {
+            if (navSubDirs_chbx.IsChecked ?? false)
+            {
+                searcher.NavSubDirectories = true;
+            }
+            else
+            {
+                searcher.NavSubDirectories = false;
+            }
+
+            if (matchWholeWord_chbx.IsChecked ?? false)
+            {
+                searcher.MatchWholeWord = true;
+            }
+            else
+            {
+                searcher.MatchWholeWord = false;
+            }
+
+            if (findInFiles_chbx.IsChecked ?? false)
+            {
+                searcher.FindInFiles = true;
+            }
+            else
+            {
+                searcher.FindInFiles = false;
+            }
+
+            if (matchCase_chbx.IsChecked ?? false)
+            {
+                searcher.MatchCase = true;
+            }
+            else
+            {
+                searcher.MatchCase = false;
+            }
+
+
+        }
+
+
+        public void SetErrorMessage(string errorMsg)
+        {
+            error_lbl.Content = errorMsg;
+        }
+
+        private void CheckEnterHandler(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if ((e.Key == Key.Return || e.Key == Key.Enter) && search_btn.IsEnabled)
+            {
+                search_btn_Click(sender, e);
+            }
+
+        }
+
+        private void FoundFiles_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ListBoxItem lbi = (ListBoxItem)(foundFiles_lstbx.ItemContainerGenerator.ContainerFromIndex(foundFiles_lstbx.SelectedIndex));
+
+            try
+            {
+                Process.Start((lbi.Content.ToString()));
+                statusError_lbl.Content = "";
+            }
+            catch (Exception ex)
+            {
+                statusError_lbl.Content = "Error: Couldn't Open File";
+
+            }
+        }
+    }
+}
---
+++ File Find/Searcher.cs
@@ -0,0 +1,466 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+
+namespace File_Find
+{
+    class Searcher
+    {
+        private string searchDirectory;
+        private string searchWord;
+        private string fileType;
+        private bool navSubDirectories;
+        private bool matchWholeWord;
+        private bool findInFiles;
+        private bool matchCase;
+        private string[] foundFiles;
+
+        public string errorMessage;
+
+        public Searcher()
+        {
+            this.searchDirectory = null;
+            this.SearchWord = null;
+            this.fileType = null;
+            this.navSubDirectories = false;
+            this.matchWholeWord = false;
+            this.findInFiles = false;
+            this.matchCase = false;
+            this.foundFiles = new string[] { };
+        }
+
+        public Searcher(string srchhDir, string srchWord, string fileTyp, bool navSubDirs, bool matchWholeWrd, bool findInFls, bool matchCase)
+        {
+            this.searchDirectory = srchhDir;
+            this.SearchWord = srchWord;
+            this.fileType = fileTyp;
+            this.navSubDirectories = navSubDirs;
+            this.matchWholeWord = matchWholeWrd;
+            this.findInFiles = findInFls;
+            this.matchCase = matchCase;
+            this.foundFiles = new string[] { };
+
+        }
+
+        public string SearchWord { get => searchWord; set => searchWord = value; }
+        public string SearchDirectory { get => searchDirectory; set => searchDirectory = value; }
+        public string FileType { get => fileType; set => fileType = value; }
+        public bool NavSubDirectories { get => navSubDirectories; set => navSubDirectories = value; }
+        public bool MatchWholeWord { get => matchWholeWord; set => matchWholeWord = value; }
+        public bool FindInFiles { get => findInFiles; set => findInFiles = value; }
+        public bool MatchCase { get => matchCase; set => matchCase = value; }
+        public string[] FoundFiles { get => foundFiles; set => foundFiles = value; }
+
+
+        public void SearchForFiles()
+        {
+            try
+            {
+                this.errorMessage = "";
+                ClearFoundFiles();
+
+                if (!Directory.Exists(this.searchDirectory))
+                {
+
+                    this.errorMessage = "Error: Directory Doesn't Exist!";
+
+                }
+                else
+                {
+
+                    string searchFile = "";
+
+
+                    if (this.MatchWholeWord)
+                    {
+                        searchFile = this.SearchWord + this.FileType;
+                    }
+                    else
+                    {
+                        searchFile = this.SearchWord.Trim() + "*" + this.FileType;
+                    }
+
+                    if (!this.MatchCase)
+                    {
+                        this.SearchWord = this.SearchWord.ToLower();
+                    }
+
+
+
+                    try
+                    {
+                        string[] found;
+                        //Find files that match the name
+                        found = Directory.GetFiles(this.SearchDirectory, searchFile, (this.NavSubDirectories ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly));
+
+                        if (this.MatchCase)
+                        {
+                            var caseList = new List<string>(found);
+                            foreach (string file in found)
+                            {
+                                if (!file.Contains(this.SearchWord))
+                                {
+                                    caseList.Remove(file);
+                                }
+                            }
+                            found = caseList.ToArray();
+                        }
+
+                        if (FindInFiles)
+                        {
+                            //Search within file type
+                            string[] CheckFiles = Directory.GetFiles(this.SearchDirectory, "*" + this.fileType, (this.NavSubDirectories ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly));
+
+                            string[] textInFile = CheckInFiles(CheckFiles);
+
+                            this.FoundFiles = found.Union(textInFile).ToArray<string>();
+
+
+                        }
+                        else
+                        {
+                            this.FoundFiles = found;
+                        }
+
+
+                    }
+                    catch (Exception UnauthorizedAccessException)
+                    {
+                        this.errorMessage = "Error: Unauthorized access to a file and/or folder that was scanned";
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                //this.errorMessage = e.ToString();
+                this.errorMessage = "Unhandled Error: " + e.ToString();
+            }
+        }
+
+        public string GetFileType(string lngFileType)
+        {
+            string fileType;
+
+            switch (lngFileType)
+            {
+                case "Text File (*.txt)":
+                    fileType = ".txt";
+                    break;
+
+                case "Word Document (*.docx)":
+                    fileType = ".docx";
+                    break;
+
+                case "Excel Workbook (*.xlsx)":
+                    fileType = ".xlsx";
+                    break;
+
+                case "CSV (*.csv)":
+                    fileType = ".csv";
+                    break;
+
+                case "All Files (*.*)":
+                    fileType = ".*";
+                    break;
+
+                default:
+                    fileType = ".*";
+                    break;
+
+            }
+            return fileType;
+        }
+
+        private string[] CheckInFiles(string[] checkFiles)
+        {
+            string[] results = new string[] { };
+            switch (FileType)
+            {
+                case ".txt":
+                case ".csv":
+                case ".*":
+                    results = CheckTextFile(checkFiles);
+                    break;
+                case ".docx":
+                    results = CheckWordFile(checkFiles);
+                    break;
+                case ".xlsx":
+                    results = CheckExcelFile(checkFiles);
+                    break;
+
+            }
+
+            return results;
+        }
+
+        private string[] CheckWordFile(string[] checkFiles)
+        {
+            var matches = new List<string>();
+            string[] returnAray;
+
+            Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+            Microsoft.Office.Interop.Word.Document doc = new Microsoft.Office.Interop.Word.Document();
+
+            foreach (string file in checkFiles)
+            {
+                object fileName = file;
+                // Define an object to pass to the API for missing parameters
+                object missing = System.Type.Missing;
+
+                try
+                {
+                    doc = word.Documents.Open(ref fileName,
+                            ref missing, ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing);
+                    string ReadValue = string.Empty;
+                    // Activate the document
+                    doc.Activate();
+
+
+                    foreach (Microsoft.Office.Interop.Word.Range tmpRange in doc.StoryRanges)
+                    {
+                        if (this.MatchCase)
+                        {
+                            ReadValue += tmpRange.Text;
+                        }
+                        else
+                        {
+                            ReadValue += tmpRange.Text.ToLower();
+                        }
+
+                    }
+
+                    if (this.MatchWholeWord)
+                    {
+                        string searchRegex = @"(^|\s)" + this.SearchWord + @"(\s|$)";
+                        if (Regex.IsMatch(ReadValue, searchRegex))
+                        {
+                            matches.Add(file);
+                        }
+                    }
+                    else
+                    {
+                        if (ReadValue.Contains(this.SearchWord))
+                        {
+                            matches.Add(file);
+                        }
+                    }
+
+                    doc.Close();
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(doc);
+                }
+
+                catch (Exception e)
+                {
+                    //Skip file
+                    //could be a temporary word file that is created when opening word files
+                }
+
+            }
+
+            return (returnAray = matches.Select(i => i.ToString()).ToArray());
+
+        }
+
+        private void ClearFoundFiles()
+        {
+            Array.Clear(this.FoundFiles, 0, this.FoundFiles.Length);
+        }
+
+        private string[] CheckTextFile(string[] filesToCheck)
+        {
+            var matches = new List<string>();
+            string[] returnAray;
+
+
+            foreach (string file in filesToCheck)
+            {
+                try
+                {
+                    //For file types not listed on the dropdown, try and read them like text files
+                    //If we cant, just skip them
+                    string fileContents;
+                    using (StreamReader streamReader = new StreamReader(file, Encoding.UTF8))
+                    {
+                        if (this.MatchCase)
+                        {
+                            fileContents = streamReader.ReadToEnd();
+                        }
+                        else
+                        {
+                            fileContents = streamReader.ReadToEnd().ToLower();
+                        }
+                    }
+
+                    if (this.MatchWholeWord)
+                    {
+                        string searchRegex = @"(^|\s)" + this.SearchWord + @"(\s|$)";
+                        if (Regex.IsMatch(fileContents, searchRegex))
+                        {
+                            matches.Add(file);
+                        }
+                    }
+                    else
+                    {
+                        if (fileContents.Contains(this.SearchWord))
+                        {
+                            matches.Add(file);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    //pass - we couldn't read the file
+                }
+
+            }
+
+            return (returnAray = matches.Select(i => i.ToString()).ToArray());
+
+        }
+
+        private string[] CheckExcelFile(string[] filesToCheck)
+        {
+            var matches = new List<string>();
+            string[] returnAray;
+
+            foreach (string file in filesToCheck)
+            {
+                DataSet data = OpenExcelFile(file);
+
+                foreach (System.Data.DataTable table in data.Tables)
+                {
+
+                    foreach (DataRow dr in table.Rows)
+                    {
+                        foreach (var item in dr.ItemArray)
+                        {
+                            string temp;
+                            if (this.MatchCase)
+                            {
+                                temp = item.ToString().Trim();
+                            }
+                            else
+                            {
+                                temp = item.ToString().Trim().ToLower();
+                            }
+
+
+                            if (this.MatchWholeWord)
+                            {
+                                string searchRegex = @"(^|\s)" + this.SearchWord + @"(\s|$)";
+                                if (Regex.IsMatch(temp, searchRegex))
+                                {
+                                    matches.Add(file);
+                                }
+                            }
+                            else
+                            {
+                                if (temp.Contains(this.SearchWord))
+                                {
+                                    matches.Add(file);
+
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+            return (returnAray = matches.Select(i => i.ToString()).ToArray());
+        }
+
+        private DataSet OpenExcelFile(string file)
+        {
+
+            string connectionString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + file + "; Extended Properties = 'Excel 12.0;HDR=NO;IMEX=1;'";
+            string[] excelsheetNames = GetExcelSheetNames(connectionString);
+
+            DataSet excelData = Parse(connectionString);
+            return excelData;
+        }
+
+        private string[] GetExcelSheetNames(string connectionString)
+        {
+
+            try
+            {
+                OleDbConnection con = null;
+                System.Data.DataTable dt = null;
+                con = new OleDbConnection(connectionString);
+                con.Open();
+                dt = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                if (dt == null)
+                {
+                    return null;
+                }
+
+                String[] excelSheetNames = new String[dt.Rows.Count];
+                int i = 0;
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    excelSheetNames[i] = row["TABLE_NAME"].ToString();
+                    i++;
+
+                }
+
+                return excelSheetNames;
+            }
+            catch (Exception e)
+            {
+                this.errorMessage = "";
+                return null;
+            }
+        }
+        public DataSet Parse(string connectionString)
+        {
+
+            try
+            {
+                DataSet data = new DataSet();
+
+                int sheetCounter = 0;
+                foreach (var sheetName in GetExcelSheetNames(connectionString))
+                {
+                    using (OleDbConnection con = new OleDbConnection(connectionString))
+                    {
+                        var dataTable = new System.Data.DataTable();
+
+                        string query = string.Format("SELECT * FROM [{0}]", sheetName);
+                        con.Open();
+                        OleDbDataAdapter adapter = new OleDbDataAdapter(query, con);
+
+                        adapter.Fill(dataTable);
+                        data.Tables.Add(dataTable);
+
+                        data.Tables[sheetCounter].TableName = sheetName;
+
+                        sheetCounter += 1;
+
+
+                    }
+                }
+
+                return data;
+            }
+            catch (Exception e)
+            {
+                this.errorMessage = "";
+                return null;
+            }
+        }
+
+    }
+}
